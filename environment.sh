c3ed55e12306b8d1c6db415960d8a8b5a43a5ace
william kennedy <mr.wjkennedy@gmail.com>
2016-11-30 11:55:21 -0800

    kvm/maas bootstrap script

---
+++ environment.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Sun Nov 27 18:30:28 PST 2016
+#
+##################################################################
+
+echo "Creating KVM virtual networks.."
+
+# "External" network
+virsh net-create networks/external.xml
+virsh net-info external
+
+# MAAS network
+virsh net-create networks/maas.xml
+virsh net-info maas
+
+echo "Creating VM for Juju.."
+
+virt-install --name juju --ram 500 --disk path=/var/lib/libvirt/images/juju.img,size=2 -f qcow2 --network network:virbr0,mac=00:00:00:00:00:ea --accelerate 
