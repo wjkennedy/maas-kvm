804b0177e320af9fa24693739239cd170f712662
William Kennedy <william@a9group.net>
2016-12-05 12:46:45 -0800

    environment.sh - PXE enabled for juju VM

--- environment.sh
+++ environment.sh
@@ -14,5 +14,4 @@ virsh net-create networks/maas.xml
 virsh net-info maas
 
 echo "Creating VM for Juju.."
-
-virt-install --name juju --ram 500 --disk path=/var/lib/libvirt/images/juju.img,size=2 -f qcow2 --network network:virbr0,mac=00:00:00:00:00:ea --accelerate 
+virt-install --name juju --ram 500 --disk path=/var/lib/libvirt/images/juju.img,size=2 -f qcow2 --network network:virbr0,mac=00:00:00:00:00:ea --accelerate --pxe
